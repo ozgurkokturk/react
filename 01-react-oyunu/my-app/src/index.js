14817d3b0db2e51d3e27ef5ff7e80423008cc829
unknown <ozgurkokturk0@gmail.com>
2021-11-17 16:09:49 +0300

    devamke

--- 01-react-oyunu/my-app/src/index.js
+++ 01-react-oyunu/my-app/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
+var durum = true;
+
+
 function calculateWinner(squares) {
-  console.log(squares);
+  var donguSayisi = JSON.stringify(squares);
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,120 +21,138 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
 
-    console.log('\n' + 'square A: ' + squares[a]);
-    console.log('square B: ' + squares[b]);
-    console.log('square C: ' + squares[c]);
-
-    if (squares[a] === squares[b] && squares[b] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      alert("Biri Kazandı valla");
+      // Kazanan: true  yazmasını istemiyorsan  return true yerine aşağıdakini return et :)
       return squares[a];
     }
+    // var donguSayisi = concat[( '%cA: ' + squares[a] + '        B: ' + squares[b] + '         C: ' + squares[c])];
   }
+
+
+  if (durum == true) console.log(donguSayisi);
+  durum = !durum;
+
   return null;
 }
 
 
-  /* Square Fonksiyonu */
-  function Square(props){
+
+
+
+/* Square Fonksiyonu */
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+/* --- Square Fonksiyonu --- */
+
+
+
+
+class Board extends React.Component {
+  renderSquare(i) {
     return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
     );
   }
-  /* --- Square Fonksiyonu --- */
 
-
-
-  class Board extends React.Component {
-
-    handleClick(i){
-        const squares = this.state.squares.slice();
-
-        if(calculateWinner(squares) || squares[i]){
-          return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-    
-
-    renderSquare(i) {
-      return (
-            <Square
-              value = {this.props.squares[i]}
-              onClick = {() => this.props.onClick(i)}
-            />
-        );
-    }
-
-    
-  
-    render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner){
-        status = 'Kazanan: ' + winner;
-      } else{
-        status = 'Sıradaki Oyuncu: ' + (this.state.xIsNext ? "X" : "O");
-      }
-
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
-  
-  class Game extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-          squares: Array(9).fill(null),
-          xIsNext: true,
-      };
+}
+
+
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
   };
 
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
 
-
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+    // squares[i] = null olarak  geliyor
+    // Eğer seçtiğimiz bir kareyi tekrar seçersek bu sefer dolu geliyor
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
-  
+
+  render() {
+    const history = this.state.history;
+    // console.log((history));
+    const current = history[history.length - 1];
+    // console.log(current);
+
+
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Kazanan: ' + winner;
+    } else {
+      status = 'Sıradaki Oyuncu: ' + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
